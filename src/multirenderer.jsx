28789a58291b38cf981578da0a8c4316eb535963
Phillip Lemons <philliplemons512@gmail.com>
2015-07-21 17:42:57 -0700

    Adding a page to test the multirenderer
    Test Plan:
    - Go to /multirenderer.html
    - See "Hello World! I am a MultiRenderer."
    
    Reviewers: aria, johnsullivan
    
    Reviewed By: johnsullivan
    
    Subscribers: kevindangoor
    
    Differential Revision: https://phabricator.khanacademy.org/D19707

---
+++ src/multirenderer.jsx
@@ -0,0 +1,12 @@
+
+var MultiRenderer = React.createClass({
+    render: function() {
+        return (
+            <div className="MultiRenderer">
+                Hello, world! I am a MultiRenderer.
+            </div>
+        );
+    }
+});
+
+module.exports = MultiRenderer;
