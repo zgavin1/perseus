01d9eaa01c7a1bc78a982482914ea5cb9617539f
brownhead <johnsullivan@khanacademy.org>
2015-07-23 12:03:37 -0700

    Add question numbers to multi-renderer.
    Summary:
    Trello card: https://trello.com/c/gPllfvVE
    
    {F160879}
    
    Test Plan:
    1) Go to http://localhost:9000/multirenderer.html
    2) See beautiful question numbers appear!
    
    Reviewers: phillip
    
    Reviewed By: phillip
    
    Subscribers: aria, kevindangoor
    
    Differential Revision: https://phabricator.khanacademy.org/D19777

--- src/multirenderer.jsx
+++ src/multirenderer.jsx
@@ -2,8 +2,19 @@ var Renderer = require("./renderer.jsx");
 
 var MultiRenderer = React.createClass({
     propTypes: {
-       itemList: React.PropTypes.arrayOf(React.PropTypes.object),
-       leftColumn: React.PropTypes.object
+        itemList: React.PropTypes.arrayOf(React.PropTypes.object),
+        leftColumn: React.PropTypes.object,
+
+        // An object that describes how to render question numbers. If falsey,
+        // no question numbers will be rendered.
+        questionNumbers: React.PropTypes.shape({
+            // The number of the first question we'll display
+            start: React.PropTypes.number.isRequired,
+
+            // The "total number of questions". This number will appear at the
+            // end of each question number (ex: "Question 2 of 7").
+            totalQuestions: React.PropTypes.number.isRequired,
+        }),
     },
 
     getInitialState: function() {
@@ -21,22 +32,46 @@ var MultiRenderer = React.createClass({
         this.setState({leftColumnRendered: true});
     },
 
-    render: function() {
-        var rendererList = null;
+    /**
+     * Renders a question number (to be placed above a question).
+     *
+     * relativeQuestionNumber will be 0 for the first question we're rendering,
+     * 1 for the second question, etc.
+     */
+    _renderQuestionNumber: function(relativeQuestionNumber) {
+        if (!this.props.questionNumbers) {
+            return null;
+        }
 
+        var num = this.props.questionNumbers.start + relativeQuestionNumber;
+        var total = this.props.questionNumbers.totalQuestions;
+        return <div key={`question-number-${relativeQuestionNumber}`}>
+            Question {num} of {total}
+        </div>;
+    },
+
+    render: function() {
         // We render in two passes, see comment in getInitialState for more
         // information.
+        var rendererList = null;
         if (this.state.leftColumnRendered) {
-            rendererList = _.map(this.props.itemList, (item, i) => {
-                // TODO (phillip): Think of a better key for the Renderer
-                return (
+            rendererList = _.flatten(_.map(this.props.itemList, (item, i) => {
+                // TODO (phillip): Think of a better key for the Renderer. This
+                //    might get us into some weird situations when we get a new
+                //    list of items (ideally we'd have React unmount everything
+                //    previously there, and mount new stuff).
+                // This two-list will be flattened by the outer call to
+                // _.flatten.
+                return [
+                    this._renderQuestionNumber(i),
                     <Renderer
                         key={i}
                         interWidgets={this._interWidgets}
                         content={item.question.content}
                         images={item.question.images}
-                        widgets={item.question.widgets} />);
-            });
+                        widgets={item.question.widgets} />,
+                ];
+            }), true);
         }
 
         return <div className="MultiRenderer">
