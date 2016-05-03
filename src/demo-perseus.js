e28d529fa745ac90547c041d87df9d50a09b4b46
Shiranka Miskin <shiranka.miskin@gmail.com>
2016-05-03 12:16:46 -0700

    Moved much of index.html/test.html into js files
    Summary: Moved js code in index.html into editor-demo.jsx and demo-index.js
    
    Test Plan: Open localhost:9000
    
    Reviewers: emily
    
    Subscribers: jared
    
    Differential Revision: https://phabricator.khanacademy.org/D27052

--- src/demo-perseus.js
+++ src/demo-perseus.js
@@ -63,17 +63,25 @@ const Perseus = window.Perseus = require('./editor-perseus.js');
 const ReactDOM = window.ReactDOM = React.__internalReactDOM;
 
 const EditorDemo = require('./editor-demo.jsx');
+const RendererDemo = require('./renderer-demo.jsx');
 
 const query = Perseus.Util.parseQueryString(window.location.hash.substring(1));
 const question = query.content ? JSON.parse(query.content) : undefined;
 const problemNum = Math.floor(Math.random() * 100);
 
+// React router v20XX
+const pathname = window.location.pathname;
+const routes = { // The value is spread across a React.createElement call
+    '/renderer': [RendererDemo],
+    '/': [EditorDemo, {question, problemNum}],
+};
+
 Perseus.init({skipMathJax: false}).then(function() {
     _DemoComponent = ReactDOM.render(
-        React.createElement(EditorDemo, {question, problemNum}),
+        React.createElement(...(routes[pathname] || routes['/'])),
         document.getElementById("perseus-container")
     );
 }).then(function() {
 }, function(err) {
-    console.error(err); //eslint-disable-line
+    console.error(err); // eslint-disable-line
 });
